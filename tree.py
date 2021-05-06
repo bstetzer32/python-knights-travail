ad6c248eae5aa7b61f099bd9af89924fa0bdab8b
Marshall-Diffey <marshalldiffey@yahoo.com>
2021-05-06 16:24:50 -0500

    Edit getters and setters

--- tree.py
+++ tree.py
@@ -24,20 +24,36 @@ class Node:
     def add_child(self, node):
         if node not in self._children:
             self._children.append(node) # might need node.value
+            node.parent = self
         else:
             print('Node already exists in list.')
 
     @parent.setter
     def parent(self, node):
-        if node not in self._children:
-            self._parent = node
+        # if node not in self._children:
+        # if(node != None):
+        if self._parent:
+            self._parent.remove_child(self)
+        self._parent = node
+        if self._parent:
             node.add_child(self)  # might need node.value
-        else:
-            print('Node already exists in list.')
-        
+        # else:
+        #     print('Node already exists in list.')
+
     def remove_child(self, node):
         if node in self._children:
             self._children.remove(node)
-            node._parent = None
+            node._parent
         else:
             print("Node doesn't exist in list.")
+
+
+# node1 = Node("root1")
+# node2 = Node("root2")
+# node3 = Node("root3")
+
+# node3.parent = node1
+# node3.parent = node2
+
+# print(node1.children)
+# print(node2.children)
